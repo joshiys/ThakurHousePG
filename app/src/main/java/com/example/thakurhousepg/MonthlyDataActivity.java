727adbe939605e169c8e106585af1430c74be484
Sachin <sachin.ahire@yahoo.com>
2019-04-01 15:23:54 +0530

    Modification in Monthly Data Screen

--- app/src/main/java/com/example/thakurhousepg/MonthlyDataActivity.java
+++ app/src/main/java/com/example/thakurhousepg/MonthlyDataActivity.java
@@ -1,7 +1,5 @@
 package com.example.thakurhousepg;
 
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -10,12 +8,10 @@ import android.widget.TextView;
 import java.util.Calendar;
 import java.util.Locale;
 
-import static android.graphics.Typeface.ITALIC;
-
 public class MonthlyDataActivity extends AppCompatActivity {
 
     private DataModule dataModule;
-    TextView expectedRent, rentCash, depositCash, receiptsPayment, totalOutstanding;
+    TextView expectedRent, rentCash, depositCash, rentReceipts, totalOutstanding, depositReceipts, totalDepositOutstanding;
     Button showMonth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +23,11 @@ public class MonthlyDataActivity extends AppCompatActivity {
         expectedRent = findViewById(R.id.expectedRent);
         rentCash = findViewById(R.id.rentCash);
         depositCash = findViewById(R.id.depositCash);
-        receiptsPayment = findViewById(R.id.receiptsPayment);
-        totalOutstanding = findViewById(R.id.totalOutstanding);
+        rentReceipts = findViewById(R.id.rentReceipts);
+        totalOutstanding = findViewById(R.id.totalRentOutstanding);
         showMonth = findViewById(R.id.showMonth);
+        depositReceipts = findViewById(R.id.depositReceipts);
+        totalDepositOutstanding = findViewById(R.id.totalDepositOutstanding);
 
 
         setTitle("Monthly Data");
@@ -46,9 +44,14 @@ public class MonthlyDataActivity extends AppCompatActivity {
         depositCash.setText("Rs." + dataModule.getTotalCashReceipts(Calendar.getInstance().get(Calendar.MONTH) + 1,
                 DataModule.ReceiptType.DEPOSIT));
 
-        receiptsPayment.setText("Rs." + dataModule.getTotalReceivedAmountForMonth(Calendar.getInstance().get(Calendar.MONTH) + 1,
+        rentReceipts.setText("Rs." + dataModule.getTotalReceivedAmountForMonth(Calendar.getInstance().get(Calendar.MONTH) + 1,
                 DataModule.ReceiptType.RENT));
         totalOutstanding.setText("Rs." + String.valueOf(dataModule.getTotalPendingAmount(DataModule.PendingType.RENT)));
+
+        depositReceipts.setText("Rs." + dataModule.getTotalReceivedAmountForMonth(Calendar.getInstance().get(Calendar.MONTH) + 1,
+                DataModule.ReceiptType.DEPOSIT));
+
+        totalDepositOutstanding.setText("Rs." + String.valueOf(dataModule.getTotalPendingAmount(DataModule.PendingType.DEPOSIT)));
     }
 
     @Override
