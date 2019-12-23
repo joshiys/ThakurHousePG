e9e94c055abe37ef3e702674543d76865c210b2c
Sachin <sachin.ahire@yahoo.com>
2019-12-23 15:18:50 +0530

    Minor changes in constants

--- app/src/main/java/com/sanyog/thakurhousepg/Constants.java
+++ app/src/main/java/com/sanyog/thakurhousepg/Constants.java
@@ -18,10 +18,10 @@ public final class Constants {
     public static final String THAKURHOUSE_DATE_FORMAT = "yyyy-MM-dd";
 
     //    public static final String  THAKURHOUSEPG_BASE_URL = "http://10.0.2.2:8080/";
-    public static final String  THAKURHOUSEPG_BASE_URL = "http://192.168.0.100:8080/";
+    public static final String  THAKURHOUSEPG_BASE_URL = "http://192.168.1.100:8080/";
 //    public static final String  THAKURHOUSEPG_BASE_URL = "http://223.229.149.129:8080/";
 //    public static final String  THAKURHOUSEPG_BASE_URL = "http://sahire.ddns.net:8080/";
-//    public static final String  THAKURHOUSEPG_BASE_URL = "http://122.170.7.152:8080/";
+//    public static final String  THAKURHOUSEPG_BASE_URL = "http://122.169.47.48:8080/";
 
 
 
